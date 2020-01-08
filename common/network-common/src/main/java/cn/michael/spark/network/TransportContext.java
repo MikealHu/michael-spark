4f38ca04bed48681323c9eea7aa08467fea7b599
hufenggang <hufenggang2019@gmai.com>
2020-01-08 11:21:58 +0800

    TransportContext read

--- common/network-common/src/main/java/cn/michael/spark/network/TransportContext.java
+++ common/network-common/src/main/java/cn/michael/spark/network/TransportContext.java
@@ -28,6 +28,16 @@ import java.util.List;
  * author: hufenggang
  * email: hufenggang2019@gmail.com
  * date: 2019/12/26 11:37
+ *
+ * 包含用于创建{@link TransportServer},{@link TransportClientFactory}以及使用
+ * {@link TransportChannelHandler}设置Netty Channel管道的上下文。
+ *
+ * TransportClient提供两种通信协议，控制层的RPCs和数据层的"chunk fetching"。
+ * RPC的处理是在TransportContext的范围之外执行的（即由用户提供的处理程序执行的），
+ * 它负责设置可以使用零拷贝IO在数据层以块流式传输的流。
+ *
+ * TransportServer和TransportClientFactory都创建了一个针对每一个管道的TransportChannelHandler。
+ * 由于每一个TransportChannelHandler包含了一个TransportClient，因此服务器进程可以利用现有管道将消息发送回客户端。
  */
 public class TransportContext implements Closeable {
     private static final Logger logger = LoggerFactory.getLogger(TransportContext.class);
@@ -38,9 +48,16 @@ public class TransportContext implements Closeable {
     // Shuffle服务的注册连接数
     private Counter registeredConnections = new Counter();
 
+    /**
+     * 强制创建MessageEncoder和MessageDecoder，以便我们确保将当前上下文类加载器切好到ExecutorClassLoader之前创建他们。
+     */
     private static final MessageEncoder ENCODER = MessageEncoder.INSTANCE;
     private static final MessageDecoder DECODER = MessageDecoder.INSTANCE;
 
+    /**
+     * 用于处理ChunkFetchRequest的单独线程池。 这有助于启用限制最大数量的TransportServer工作线程，
+     * 这些线程在通过基础通道将ChunkFetchRequest消息的响应写回到客户端时被阻止。
+     */
     private final EventLoopGroup chunkFetchWorkers;
 
     public TransportContext(TransportConf conf, RpcHandler rpcHandler) {
@@ -60,7 +77,9 @@ public class TransportContext implements Closeable {
      * @param conf                 TransportConf对象
      * @param rpcHandler           RpcHandler负责处理请求和响应
      * @param closeIdleConnections 如果设置为true，则关闭空闲连接
-     * @param isClientOnly
+     * @param isClientOnly         该配置表示TransportContext仅通过一个客户端使用。
+     *                             启动外部shuffle的时候，该配置是很重要的，它停止为shuffle客户端
+     *                             创建额外的事件循环和后续线程池，以处理分块的提取请求。
      */
     public TransportContext(
             TransportConf conf,
@@ -83,6 +102,13 @@ public class TransportContext implements Closeable {
         }
     }
 
+    /**
+     * 初始化运行给定TransportClientBootstraps的ClientFactory，然后返回一个新的客户端。
+     * 引导程序必须同步执行，并且必须成功创建后才可以创建客户端。
+     *
+     * @param bootstraps
+     * @return
+     */
     public TransportClientFactory createClientFactory(List<TransportClientBootstrap> bootstraps) {
         return new TransportClientFactory(this, bootstraps);
     }
@@ -91,18 +117,18 @@ public class TransportContext implements Closeable {
         return createClientFactory(new ArrayList<>());
     }
 
+    /** 创建一个服务器用于尝试连接具体端口 */
     public TransportServer createServer(int port, List<TransportServerBootstrap> bootstraps) {
         return new TransportServer(this, null, port, rpcHandler, bootstraps);
     }
 
+    /** 创建一个服务器用于尝试连接具体主机地址+端口 */
     public TransportServer createServer(
             String host, int port, List<TransportServerBootstrap> bootstraps) {
         return new TransportServer(this, host, port, rpcHandler, bootstraps);
     }
 
-    /**
-     * Creates a new server, binding to any available ephemeral port.
-     */
+    /** 创建一个新的服务器, 连接一些可用的临时端口。*/
     public TransportServer createServer(List<TransportServerBootstrap> bootstraps) {
         return createServer(0, bootstraps);
     }
@@ -117,15 +143,15 @@ public class TransportContext implements Closeable {
 
     /**
      * 初始化客户端或服务器Netty Channel管道，该管道对消息进行编码/解码，
-     * 并具有{@link cn.michael.spark.network.server.TransportChannelHandler}来处理请求或响应消息。
+     * 并具有{@link TransportChannelHandler}来处理请求或响应消息。
      * <p>
      * 来自网络的请求首先经过TransportFrameDecoder进行粘包拆包，然后将数据传递给MessageDecoder，
      * 由MessageDecoder进行解码，之后再经过IdleStateHandler来检查Channel空闲情况，
      * 最终将解码了的消息传递给TransportChannelHandler。
      * 在TransportChannelHandler中根据消息类型的不同转发给不同的处理方法进行处理，将消息发送给上层的代码。
      *
-     * @param channel
-     * @param channelRpcHandler
+     * @param channel 需要初始化的管道
+     * @param channelRpcHandler 用于管道的RPC 处理程序
      * @return 返回创建的TransportChannelHandler，该对象包括一个被用于在管道上消息传递的TransportClient
      * TransportClient直接与ChannelHandler关联，以确保同一通道的所有用户都获得相同的TransportClient对象。
      */
